cf8b0f189a177cc5e83e3bf2447ef0aae1b09066
ibukunolatunde1 <ibukunolatunde1@gmail.com>
2018-08-22 19:20:14 +0100

    Edits to file paths

--- server/config/config.js
+++ server/config/config.js
@@ -3,23 +3,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const dbConfig = {
-    development: {
-        user: process.env.USER,
-        host: process.env.HOST,
-        database: process.env.DB,
-        password: process.env.PASSWORD,
-        port: process.env.MY_PORT,
-    },
-    test: {
-        user: process.env.USER,
-        host: process.env.HOST,
-        database: process.env.DB_TEST,
-        password: process.env.PASSWORD,
-        port: process.env.MY_PORT,
-    },
-    production: {
-        production_connection: 'DATABASE_URL',
-    },
+  development: {
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DB,
+    password: process.env.PASSWORD,
+    port: process.env.MY_PORT,
+  },
+  test: {
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DB_TEST,
+    password: process.env.PASSWORD,
+    port: process.env.MY_PORT,
+  },
+  production: {
+    production_connection: 'DATABASE_URL',
+  },
 };
 
 export default dbConfig;
--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
-import config from '../config/config';
+import config from './config';
 
 dotenv.config();
 
@@ -8,22 +8,22 @@ const env = process.env.NODE_ENV || 'development';
 const connection = config[env];
 
 let poolConfig;
-if(connection.production_connection) {
+if (connection.production_connection) {
   poolConfig = new Pool({
     connectionString: process.env[connection.production_connection],
   });
-}else {
+} else {
   poolConfig = new Pool(connection);
 }
 
 const pool = poolConfig;
 
 pool.connect((err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Connected');
-    }
-  });
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Connected');
+  }
+});
 
 export default pool;
